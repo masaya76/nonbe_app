7ad0d5207ce7b6b90f3c941e8e1878b9d6fa0f5d
EC2 Default User <ec2-user@ip-172-31-30-18.ap-northeast-1.compute.internal>
2022-05-07 20:03:34 +0000

    [update]投稿機能ーTagモデルsave-postsメソッドのold_tagsの削除, menber/mypage.htmlの遷移先link to post_path(post_image)=>(post)へ変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -17,21 +17,19 @@ class Post < ApplicationRecord
 
   def save_posts(sent_tags)  #今回のタグの付け方は既存するタグデータを一回削除＝＞新規作成
     current_post_tags = Tag.where(tag_name: sent_tags)
-    old_tags = current_post_tags - sent_tags   #   既存するタグ     = 例） 1, 2, 3,(db内にあるタグ) - 1,(送られてきたタグ)
-    new_tags = sent_tags - current_post_tags   #dbに存在しないタグ  = 例） 4(送られてきたタグ) - 1, 2, 3 (db内にあるタグ)
 
-    old_tags.each do |old|     #18行目～20行目データ消す
-      self.post_tags.delete PostTag.find_by(tag_name: old)
-    end
+    new_tags = sent_tags - current_post_tags   #dbに存在しないタグ  = 例） 4(送られてきたタグ) - 1, 2, 3 (db内にあるタグ)
 
     new_tags.each do |new|   #old_tags.each do |old|で消した後に新しいデータを作成
       if Tag.exists?(tag_name:new)  #tag確認＝＞dbに作成済みのtag_nameが存在する
         @tag = Tag.find_by(tag_name:new) #存在している＝＞tag_nameからtagモデルを呼ぶ(find_by)
         new_post_tag = PostTag.create(post_id:self.id,tag_id: @tag.id)  #post_id:self  自分自身の情報泳げるように（自在に移動できるように）＝＞インスタンス変数にする必要がない
+        binding.pry
       else
         @tag = Tag.create(tag_name:new)  #tagが存在しない＝＞存在しないtag_name作成
         new_post_tag = PostTag.create(post_id:self.id,tag_id: @tag.id)
         self.post_tags << new_post_tag   #self.左辺に右辺のデータを破壊的に追加
+
       end
     end
   end
@@ -49,6 +47,4 @@ class Post < ApplicationRecord
       end
     end
   end
-
-
 end
